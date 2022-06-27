34c6ccf1ad9a1248c341d60bc10a5cb21558adf8
Oussama Mohamed <ous-honor@live.fr>
2022-06-27 16:56:18 +0200

    Autorisation GPS

--- GRando/ViewControllerMap.swift
+++ GRando/ViewControllerMap.swift
@@ -6,12 +6,66 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewControllerMap: UIViewController {
+class ViewControllerMap: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    
+    let manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        checkLocationServices()
 
+    }
+    
+    func checkLocationServices(){
+        if CLLocationManager.locationServicesEnabled(){
+            manager.delegate = self
+            manager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+            checkLocationAuthorization()
+        } else {
+            checkLocationServices()
+        }
+    }
+    
+    func checkLocationAuthorization(){
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            manager.startUpdatingLocation()
+        case .denied:
+            break
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            break
+        case .authorizedAlways:
+            break
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthorization()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            manager.stopUpdatingLocation()
+            
+            render(location)
+        }
+    }
+    
+    func render(_ location: CLLocation){
+        
+        
     }
     
 }
